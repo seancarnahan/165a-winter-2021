ab03e7eaabb0ce5d4be838a39ff3ea422a9432e3
Gabriel Vazquez <gabvazquez@ucdavis.edu>
2021-02-12 17:26:46 -0800

    Adding docstrings to page.py

--- template/page.py
+++ template/page.py
@@ -1,26 +1,35 @@
 from template.config import *
 
-#can store 4000kb
-#4 bytes is allocated for each record
-#1000 max records per page
+
 class Page:
+    """
+    A Page Object stores the actual data.
+
+    The number of records per Page is determined by PAGE_SIZE found in config.py
+
+    Every entry into the page is 4 bytes.
+
+    In total, a Page can store 4000 kb of data.
+    """
 
     def __init__(self):
         self.num_records = 0 #max: 1000
         self.data = bytearray()
-    
-    #if num_records < 1000 return true
-    #if num_records >= 1000 return false
+
     def has_capacity(self):
+        """ Return True if Page has capacity, otherwise return False """
         if self.num_records >= PAGE_SIZE:
             return False
         else:
             return True
 
-    #input: integer
-    #output: if page still has room return true, else return false
-    #adds an integer to a page 
     def write(self, value):
+        """
+        Write value to the page.
+
+        :param value: int
+        :returns: True if write is successful otherwise False
+        """
         if self.has_capacity():
             byteArray = Page().integerToBytes(value)
 
@@ -31,9 +40,14 @@ class Page:
         else:
             return False #Failed to add record to page 
 
-    #input: int index from 0 - 1023999
-    #output: integer value of that record
     def getRecord(self, index):
+        """
+        Read a value from the page that corresponds to the given index of a record.
+
+        :param index: int
+
+        :returns: the value at the given index. Return False if read is unsuccessful.
+        """
         x1, x2 = Page().getRecordIndexes(index)
     
         try:
@@ -41,10 +55,15 @@ class Page:
         except ValueError:
             return False
 
-    #input: the index of the record to replace, new integer value
-    #output: void
     def replaceRecord(self, index, newIntVal):
+        """
+        Replace the value at index with newIntVal.
 
+        :param index: int
+        :param newIntVal: int
+
+        :returns: True if replace is successful. Otherwise False.
+        """
         if index > self.num_records:
             return False
 
@@ -52,29 +71,35 @@ class Page:
         byteArray = Page().integerToBytes(newIntVal)
         self.data[x1: x2] = byteArray
         return True
-        
-    #input: index from 0 - 999
-    #output: a list [x1,x2] (inclusive,exclusive)
-    #indexes will allow you to retreive a slice from self.data
+
     @staticmethod
     def getRecordIndexes(index):
+        """
+        Remap index to corresponding index but in increments of PAGE_RECORD_SIZE.
+
+        :param index: int
+        :returns: a list of two values. [StartOfRecord Index (inclusive), EndOfRecord Index (exclusive)]
+        """
         return [index * PAGE_RECORD_SIZE, (index+1) * PAGE_RECORD_SIZE]
 
-    #input: an interger -> must convert to 4 bytes, make sure its not too long
-    #output: the hex(integer) in a bytearray format
-    #hexed version of integer must be 8 bit?
     @staticmethod
     def integerToBytes(integer):
+        """
+        Convert an integer to 4 bytes
+
+        :param integer: int
+        :returns: bytearray containing bytes of integer.
+        """
+
+        # Hexed version of integer must be 8 bit?
         return (integer).to_bytes(4,'big')
 
-    #input:bytearray
-    #output: Hex
     @staticmethod
     def bytesToHex(bytesArray):
+        """ Converts a bytearray into hexadecimal values"""
         return bytesArray.hex()
-    
-    #input a hex string
-    #output: integer
+
     @staticmethod
     def hexToInt(hexStr):
+        """ Converts a string containing hex values into an integer """
         return int(hexStr, 16)
