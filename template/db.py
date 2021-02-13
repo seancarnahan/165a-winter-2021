9f9af5991ceb5ea2fc594e2b7905eb5b9129bd3c
Gabriel Vazquez <gabvazquez@ucdavis.edu>
2021-02-12 17:08:49 -0800

    Adding docstrings to Query and Database methods

--- template/db.py
+++ template/db.py
@@ -13,32 +13,38 @@ class Database:
     def close(self):
         pass
 
-    """
-    # Creates a new table
-    :param name: string         #Table name
-    :param num_columns: int     #Number of Columns: all columns are integer
-    :param key: int             #Index of table key in columns
-    """
-
     def create_table(self, name, num_columns, key):
+        """
+        Creates a new table
+
+        :param name: string         #Table name
+        :param num_columns: int     #Number of Columns: all columns are integer
+        :param key: int             #Index of table key in columns
+
+        :returns: The newly created table object
+        """
         table = Table(name, num_columns, key)
         self.tables.append(table)
         return table
 
-    """
-    # Deletes the specified table
-    """
-
     def drop_table(self, name):
+        """
+        Deletes the specified table
+
+        :param name: string         #Table Name
+        """
         for table in self.tables:
             if table.name == name:
                 self.tables.remove(table)
 
-    """
-    # Returns table with the passed name
-    """
-
     def get_table(self, name):
+        """
+        Returns the table with the passed name
+
+        :param name: string         #Table Name
+
+        :returns: Table object corresponding to name.
+        """
         for table in self.tables:
             if table.name == name:
                 return table
--- template/query.py
+++ template/query.py
@@ -1,5 +1,3 @@
-from template.table import Table, Record
-from template.index import Index
 from template.config import *
 
 
@@ -14,19 +12,21 @@ class Query:
     def __init__(self, table):
         self.table = table
 
-    """
-    # internal Method
-    # Read a record with specified RID
-    # Returns True upon succesful deletion
-    # Return False if record doesn't exist or is locked due to 2PL
-    When a record is deleted, the base record will be
-    invalidated by setting the RID of itself and all its tail records to a special value
-    """
     def delete(self, key):
-        # RID is for the base record
-        # schema encoding = 2 for delete
+        """
+        INTERNAL METHOD!!
+        Deletes record with matching key.
+
+        :param key: Int.        The primary key of a record.
+
+        :returns: True upon successful deletion.
+                  False if record doesn't exist or is locked due to 2PL
+        """
+
+        # When a record is deleted, the base record will be invalidated by creating a new tail record with
+        # schema encoding set as 2.
         try:
-            rid = self.table.index.locate(self.table.key, key)
+            rid = self.table.index.locate(self.table.key, key) # base record rid
         except IndexError:
             return False
 
@@ -39,27 +39,31 @@ class Query:
         except:
             return False
 
-    """
-    # Insert a record with specified columns
-    # Return True upon succesful insertion
-    # Returns False if insert fails for whatever reason
-    """
     def insert(self, *columns):
+        """
+        Insert a record with specified columns
+
+        :param columns: data to be inserted into the table.
+        :returns: True if insert operation is successful. Otherwise False
+        """
         try:
             self.table.createNewRecord(columns[0], columns)
             return True
         except:
             return False
 
-    """
-    # Read a record with specified key
-    # :param key: the key value to select records based on
-    # :param query_columns: what columns to return. array of 1 or 0 values.
-    # Returns a list of Record objects upon success
-    # Returns False if record locked by TPL
-    # Assume that select will never be called on a key that doesn't exist
-    """
     def select(self, key, column, query_columns):
+        """
+        Read a record with specified key. Assume this function will never be called on a key
+        that doesn't exist.
+
+        :param key: Int.            The key value to select records based on
+        :param column: Int.         The column to search for key
+        :param query_columns: list of 1s or 0s.  The columns to return.
+
+        :returns: List of record objects upon success. Otherwise False
+        """
+
         rids = self.table.index.locate(column, key)
         recordList = []
         try:
@@ -75,13 +79,16 @@ class Query:
         except:
             return False
 
-
-    """
-    # Update a record with specified key and columns
-    # Returns True if update is succesful
-    # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking
-    """
     def update(self, key, *columns):
+        """
+        Update a record with specified key and columns
+
+        :param key: Int.            The primary key of record to update.
+        :param columns: Tuple.      The column values to update. If entry is None, then it will not be changed.
+
+        :returns: True if update is successful. False if no records exist with given key or if target record cannot
+                  be accessed due to 2PL Lockings.
+        """
         # rid is the rid of base record
         rids = self.table.index.locate(self.table.key, key)
         try:
@@ -91,15 +98,17 @@ class Query:
         except:
             return False
 
-    """
-    :param start_range: int         # Start of the key range to aggregate 
-    :param end_range: int           # End of the key range to aggregate 
-    :param aggregate_columns: int  # Index of desired column to aggregate
-    # this function is only called on the primary key.
-    # Returns the summation of the given range upon success
-    # Returns False if no record exists in the given range
-    """
     def sum(self, start_range, end_range, aggregate_column_index):
+        """
+        Sums the columns specified by aggregate_column_index. Only sums records between [start_range, end_range].
+
+        :param start_range: int         # Start of the key range to aggregate
+        :param end_range: int           # End of the key range to aggregate
+        :param aggregate_column_index: int          # Desired column to aggregate. Should have an Index created
+                                                      beforehand.
+
+        :returns: The summation of the given range upon success. Otherwise False if no records exists in given range.
+        """
         ridRange = self.table.index.locate_range(start_range, end_range,
                                                  self.table.key)
 
@@ -112,16 +121,15 @@ class Query:
             sum += value
         return sum
 
-
-    """
-    incremenets one column of the record
-    this implementation should work if your select and update queries already work
-    :param key: the primary of key of the record to increment
-    :param column: the column to increment
-    # Returns True is increment is successful
-    # Returns False if no record matches key or if target record is locked by 2PL.
-    """
     def increment(self, key, column):
+        """
+        Incremented one column of the record (Note: Should work if Select & Update work)
+
+        :param key: int         # primary key of record to increment
+        :param column: int      # column to increment
+
+        :returns: True if increment is successful. False if no record matches key or if target record is locked by 2PL.
+        """
         r = self.select(key, self.table.key, [1] * self.table.num_all_columns)[0]
         if r is not False:
             updated_columns = [None] * self.table.num_all_columns
