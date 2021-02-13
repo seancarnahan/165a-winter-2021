43040bda9447eb54eb007598a68221558f17b360
Sean Carnahan <Sean.Carnahan5@gmail.com>
2021-02-13 14:18:00 -0800

    main: more changes

--- template/buffer_pool.py
+++ template/buffer_pool.py
@@ -2,8 +2,32 @@ from template.config import *
 
 class BufferPool:
     def __init__(self, pages):
+        self.size = BUFFER_POOL_NUM_BASE_PAGES
+        self.TPsSize = BUFFER_POOL_NUM_TAIL_PAGES_PER_BASE
+        self.basePages = []
+
+        #key: index on self.basePages
+        #value: list of corresponding tail pages
+        self.tailPages = {}
+
+
+    def read_from_disk(self):
+        pass
+    
+
+    def write_from_disk(self):
+        pass
+    
+
+    def create_file(self):
         pass
 
+    """
+        - determine which page in the buffer pool to evict
+        - check if the evictable page is dirty, if so update the page in disk
+    """
+    def remove_LRU_page(self):
+        pass
 
 
 
