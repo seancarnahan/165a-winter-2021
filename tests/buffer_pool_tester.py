c790217fd29c94a2b4f1a5b09c3a2a288c548c08
Sean Carnahan <Sean.Carnahan5@gmail.com>
2021-02-15 18:01:25 -0800

    main: added buffer pool tester

---
+++ tests/buffer_pool_tester.py
@@ -0,0 +1,15 @@
+import unittest
+
+from template.buffer_pool import BufferPool
+from unittest import mock
+
+class BufferPoolTester(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_create_file(self):
+        pass
+
+    def test_write_to_disk(self):
+        pass
+
